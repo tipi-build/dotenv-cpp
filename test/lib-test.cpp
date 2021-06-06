ad1637b811c197d364106bac99da2cf009c1ce7a
Lambourl <lmbrluc@gmail.com>
2021-06-06 18:19:19 +0200

    :wrench: file env is for test

--- test/lib-test.cpp
+++ test/lib-test.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
   
-  dotenv::use_dotenv_file("/Users/luclambour/Desktop/Workspace/dotenv-cpp/.env");
+  dotenv::use_dotenv_file("/Users/luclambour/Desktop/Workspace/dotenv-cpp/test/.env");
   std::cout<<std::getenv("PORT")<<std::endl;
   std::cout<<std::getenv("COUPLE")<<std::endl;
   std::cout<<std::getenv("BASE_URL")<<std::endl;
